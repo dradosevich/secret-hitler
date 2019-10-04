3567a74ab6bbce14418a2ce7d153da0dc11280e7
dradosevich <deradosevich@gmail.com>
2019-10-04 11:49:49 -0600

    first files

---
+++ client/secHitClient.py
@@ -0,0 +1,14 @@
+#Danny Radosevich
+#Client file for Secret hitler board game app
+
+#the import statement for what will be the bases of the GUI
+import tkinter
+
+
+
+#begin set up for the display
+m = tkinter.Tk()
+#add functionality here
+
+#send up screen
+m.mainLoop()
---
+++ server/secHitServer.py
@@ -0,0 +1,2 @@
+#Danny Radosevich
+# server for the secret hitler baord game app
